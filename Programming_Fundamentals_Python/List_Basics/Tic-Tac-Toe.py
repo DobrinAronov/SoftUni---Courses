61995c2b10c57632613b56f8a8a0028f1f3155f1
DobrinAronov <Dobrin.Aronov44@gmail.com>
2026-05-30 21:23:40 +0300

    Modified program Tic-Tac-Toe

--- Programming_Fundamentals_Python/List_Basics/Tic-Tac-Toe.py
+++ Programming_Fundamentals_Python/List_Basics/Tic-Tac-Toe.py
@@ -1,53 +1,39 @@
-all_movies = []
+def check_winner(a: int, b: int, c: int):
+    if a == b == c == 1:
+        return 'First'
+    elif a == b == c == 2:
+        return 'Second'
+    else:
+        return None
 
-for _ in range(3):
-    current_game = [int(number) for number in input().split()]
-    all_movies.append(current_game)
+all_rows = []
+
+for number_of_row in range(3):
+    current_row = [int(number) for number in input().split()]
+    all_rows.append(current_row)
 
 winner = ''
+
 # Checking The rows
-for row in all_movies:
-    if row == [1, 1, 1]:
-        winner = 'First'
-        break
-    elif row == [2, 2, 2]:
-        winner = 'Second'
+for row in all_rows:
+    winner = check_winner(row[0], row[1], row[2])
+    if winner:
         break
 
-# Checking The Columns
+if not winner:
+    # Checking The Columns
+    for column in range(0, 3):
+        winner = check_winner(all_rows[0][column], all_rows[1][column], all_rows[2][column])
+        if winner:
+            break
 
-if all_movies[0][0] == 1 and all_movies[1][0] == 1 and all_movies[2][0] == 1:
-    winner = 'First'
-
-elif all_movies[0][0] == 2 and all_movies[1][0] == 2 and all_movies[2][0] == 2:
-    winner = 'Second'
-
-if all_movies[0][1] == 1 and all_movies[1][1] == 1 and all_movies[2][1] == 1:
-    winner = 'First'
-
-elif all_movies[0][1] == 2 and all_movies[1][1] == 2 and all_movies[2][1] == 2:
-    winner = 'Second'
-
-if all_movies[0][2] == 1 and all_movies[1][2] == 1 and all_movies[2][2] == 1:
-    winner = 'First'
-
-elif all_movies[0][2] == 2 and all_movies[1][2] == 2 and all_movies[2][2] == 2:
-    winner = 'Second'
-
-# Checking The diagonals
-if all_movies[0][0] == 1 and all_movies[1][1] == 1 and all_movies[2][2] == 1:
-    winner = 'First'  # Check left diagonal
-elif all_movies[0][0] == 2 and all_movies[1][1] == 2 and all_movies[2][2] == 2:
-    winner = 'Second'
-
-if all_movies[0][2] == 1 and all_movies[1][1] == 1 and all_movies[2][0] == 1:
-    winner = 'First'  # Check right diagonal
-elif all_movies[0][2] == 2 and all_movies[1][1] == 2 and all_movies[2][0] == 2:
-    winner = 'Second'
+if not winner:
+    winner = check_winner(all_rows[0][0], all_rows[1][1], all_rows[2][2])  # Check left diagonal
+    if not winner:
+        winner = check_winner(all_rows[0][2], all_rows[1][1], all_rows[2][0])  # Check right diagonal
 
 if winner == 'First':
     print("First player won")
-
 elif winner == 'Second':
     print("Second player won")
 else:
