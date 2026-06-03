94029c7deaf6b0a2b7d7cdc657d57661dbab84a6
DobrinAronov <Dobrin.Aronov44@gmail.com>
2026-06-03 17:50:53 +0300

    Modified program "Longer_Line"

--- Programming_Fundamentals_Python/Functions/Longer_Line.py
+++ Programming_Fundamentals_Python/Functions/Longer_Line.py
@@ -2,6 +2,7 @@ from math import sqrt, floor
 
 
 def coordinates_closest_point(some_tuple: tuple) -> tuple:
+
     distance_first_point = sqrt(some_tuple[0][0] ** 2 + some_tuple[0][1] ** 2)
     distance_second_point = sqrt(some_tuple[1][0] ** 2 + some_tuple[1][1] ** 2)
     x = y = None
@@ -9,7 +10,6 @@ def coordinates_closest_point(some_tuple: tuple) -> tuple:
         x, y = some_tuple[0][0], some_tuple[0][1]
     elif distance_second_point < distance_first_point:
         x, y = some_tuple[1][0], some_tuple[1][1]
-
     return x, y
 
 
@@ -24,25 +24,25 @@ def longer_line(x_1_frs: float, y_1_frs: float, x_2_frs: float, y_2_frs: float,
     y_sec_min = min(y_1_sec, y_2_sec)
     y_sec_max = max(y_1_sec, y_2_sec)
 
-    distance_first_line = sqrt((x_frs_max - x_frs_min) ** 2 + (y_frs_max - y_frs_min) ** 2)
-    distance_second_line = sqrt((x_sec_max - x_sec_min) ** 2 + (y_sec_max - y_sec_min) ** 2)
+    length_first_line = sqrt((x_frs_max - x_frs_min) ** 2 + (y_frs_max - y_frs_min) ** 2)
+    length_second_line = sqrt((x_sec_max - x_sec_min) ** 2 + (y_sec_max - y_sec_min) ** 2)
 
     coordinates_first_line = ((x_1_frs, y_1_frs), (x_2_frs, y_2_frs))
     coordinates_second_line = ((x_1_sec, y_1_sec), (x_2_sec, y_2_sec))
 
-    if distance_first_line >= distance_second_line:
-        the_longest_line = coordinates_first_line
+    if length_first_line >= length_second_line:
+        the_longer_line = coordinates_first_line
     else:
-        the_longest_line = coordinates_second_line
+        the_longer_line = coordinates_second_line
 
-    closest_point = coordinates_closest_point(the_longest_line)
+    closest_point = coordinates_closest_point(the_longer_line)
 
-    if closest_point == the_longest_line[0]:
-        closest_point = the_longest_line[0]
-        farthest_point = the_longest_line[1]
+    if closest_point == the_longer_line[0]:
+        closest_point = the_longer_line[0]
+        farthest_point = the_longer_line[1]
     else:
-        closest_point = the_longest_line[1]
-        farthest_point = the_longest_line[0]
+        closest_point = the_longer_line[1]
+        farthest_point = the_longer_line[0]
 
     x_1, y_1 = [floor(element) for element in closest_point]
     x_2, y_2 = [floor(element) for element in farthest_point]
