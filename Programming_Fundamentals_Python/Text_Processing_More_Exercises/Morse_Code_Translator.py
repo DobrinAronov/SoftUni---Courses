d1d641591db57decedbeecefee3f876643fafc77
DobrinAronov <Dobrin.Aronov44@gmail.com>
2026-07-14 14:52:43 +0300

    I update the program Morse_Code_Translator.py

--- Programming_Fundamentals_Python/Text_Processing_More_Exercises/Morse_Code_Translator.py
+++ Programming_Fundamentals_Python/Text_Processing_More_Exercises/Morse_Code_Translator.py
@@ -1,22 +1,23 @@
-def morse_code_translator(word_list: list) -> str:
+def morse_code_translator(word_list: list, morse_codes: dict) -> str:
     output_message = ''
-    morse_code_letters = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
-                          'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
-                          'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-',
-                          'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'
-                          }
 
     for morse_letter in word_list:
         if morse_letter == '|':
             output_message += ' '
         else:
-            for letter, code in morse_code_letters.items():
+            for letter, code in morse_codes.items():
                 if morse_letter == code:
                     output_message += letter
                     break
     return output_message
 
 
+morse_code_alphabet = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.',
+                       'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-',
+                       'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..'
+                       }
+
 morse_code_list = input().split()
 
-print(morse_code_translator(morse_code_list))
+print(morse_code_translator(morse_code_list, morse_code_alphabet))
